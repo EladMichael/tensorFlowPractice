36300d5346ff6e6ff9106ecda2ec6b46e90a0091
Elad Michael <elad.michael@gmail.com>
2024-06-23 16:56:42 -0600

    trying the hyperparameter template on the grad boosted trees

--- titanic/predictor.py
+++ titanic/predictor.py
@@ -14,7 +14,7 @@ def main():
 	train = tfdf.keras.pd_dataframe_to_tf_dataset(pd_train,label='Survived');
 	test = tfdf.keras.pd_dataframe_to_tf_dataset(pd_test);
 
-	model = tfdf.keras.RandomForestModel(verbose=1);
+	model = tfdf.keras.RandomForestModel();
 
 	model.fit(train);
 
@@ -31,7 +31,7 @@ def main():
 	output.to_csv('data/randomSubmission.csv',index=False);
 	print("submission saved!");
 
-	model2 = tfdf.keras.GradientBoostedTreesModel(verbose=1);
+	model2 = tfdf.keras.GradientBoostedTreesModel();
 	model2.fit(train);
 	with open("plotGrad.html", "w") as f: 
 		f.write(tfdf.model_plotter.plot_model(model2))
@@ -47,6 +47,26 @@ def main():
 	output2 = pd.DataFrame({'PassengerId': pd_test.PassengerId, 'Survived': testSurvived2});
 	output2.to_csv('data/gradSubmission.csv',index=False);
 	print("submission saved!");
+
+	model3 = tfdf.keras.GradientBoostedTreesModel(hyperparameter_template="benchmark_rank1");
+	model3.fit(train);
+	with open("plotGrad2.html", "w") as f: 
+		f.write(tfdf.model_plotter.plot_model(model3))
+
+	model3.summary();
+	model3.make_inspector().variable_importances()
+
+	result3 = model3.predict(test);
+	testSurvived3 = [1 if p>=0.5 else 0 for p in result3];
+
+	print("Boosted Models differed on the predictions of: ",np.sum(np.abs(np.array(testSurvived2) - np.array(testSurvived3))))
+
+	output3 = pd.DataFrame({'PassengerId': pd_test.PassengerId, 'Survived': testSurvived3});
+	output3.to_csv('data/gradSubmissionHyper.csv',index=False);
+	print("submission saved!");
+
+	
+
 
 
 
